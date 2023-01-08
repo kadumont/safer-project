46802e441d610459b8a6fe1cc41a6fd134b897ef
kadumont <63063561+kadumont@users.noreply.github.com>
2023-01-09 00:25:17 +0100

    Ajout Crud Bien

---
+++ src/Form/BienType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Bien;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BienType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('descriptif')
+            ->add('codepostal')
+            ->add('prix')
+            ->add('surface')
+            ->add('typeachat')
+            ->add('categorie')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Bien::class,
+        ]);
+    }
+}
