28cecb6218286b9c6b5fcc1205b4429eff9bb781
kadumont <63063561+kadumont@users.noreply.github.com>
2023-01-09 03:41:09 +0100

    Création page Bien pour User connecté

--- src/Controller/BienController.php
+++ src/Controller/BienController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Bien;
+use App\Repository\BienRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+#[Route('/bien/profil', name: 'app_bien')]
 class BienController extends AbstractController
 {
-    #[Route('/bien', name: 'app_bien')]
-    public function index(): Response
+    #[Route('/', name: 'app_bien_index')]
+    public function index(BienRepository $bienRepository): Response
     {
         return $this->render('bien/index.html.twig', [
-            'controller_name' => 'BienController',
+            'biens' => $bienRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_bien_show', methods: ['GET'])]
+    public function show(Bien $bien): Response
+    {
+        return $this->render('bien/show.html.twig', [
+            'bien' => $bien,
         ]);
     }
 }
+
